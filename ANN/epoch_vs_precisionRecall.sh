a10e77880ed085ba7e081242d900168a6a776baf
Soumik Dutta <sammagnet7@gmail.com>
2023-11-28 14:05:39 +0530

    Modifies ploting of ANN

--- ANN/epoch_vs_precisionRecall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Extract and format precision and recall for each unique epoch in CSV format
-echo "Epoch,Precision,Recall" > log_precisionRecall.txt
-awk '/Epoch/ {epoch = $2} /Precision:/ {precision = $2} /Recall:/ {recall = $2; print epoch precision "," recall}' log_training.txt >> log_precisionRecall.txt
-
-python3 epoch_vs_precisionRecall.py
-
-#rm log_precisionRecall.txt
